8949839b2afc8aa0567ed49652137f0ae27cad58
Aaron A. Brown <kilonow1275@hotmail.com>
2023-09-15 21:29:53 -0600

    added validation when purchasing credits

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   // can add multiple properties if needed
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model("users", userSchema);
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,6 +4,10 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 module.exports = app => {
   // watch for post requests made to the api stripe route
   app.post("/api/stripe", async (req, res) => {
+    // ensure user is authenticated before accessing the api
+    if (!req.user) {
+      return res.status(401).send({ error: "You must log in!" });
+    }
     // generate charge
     const charge = await stripe.charges.create({
       amount: 500,
@@ -11,6 +15,11 @@ module.exports = app => {
       description: "$5 for 5 credits",
       source: req.body.id
     });
-    console.log(charge);
+    // reference the user object
+    req.user.credits += 5;
+    // save data changes to the user object
+    const user = await req.user.save();
+    // send the user object back to the client
+    res.send(user);
   });
 };
